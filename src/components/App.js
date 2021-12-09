a54bcd707dac37a0f50e7e8b1b742a8ec7fcf0fd
Amol Usare <91278057+amolusare21@users.noreply.github.com>
2021-12-09 19:55:54 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
-import React from 'react'
-import '../styles/App.css';
+import React, { useState } from "react";
+import "./styles.css";
 const App = () => {
-  
+  const [value1, setValue] = useState("Marco");
+  const [value2, setValue1] = useState("Polo");
+   function onclick(){
+     
+      setValue(value2);
+      setValue(value1);
+   }
   return (
     <div id="main">
-
+      <h1 id="marco- polo" >
+        {value1}
+      </h1>
+      <button id="marco- polo" onClick={onclick} >
+        {value2}
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
+
